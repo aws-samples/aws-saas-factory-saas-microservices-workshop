0fccd847304f91394fb87e52f9865b075f61a412
Syed Hussain <suhussai@ualberta.ca>
2022-11-23 10:19:35 -0700

    set cli pager to empty string via aws cli configure

--- setup.sh
+++ setup.sh
@@ -45,6 +45,7 @@ echo "export AWS_REGION=${AWS_REGION}" | tee -a ~/.bash_profile
 echo "export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}" | tee -a ~/.bash_profile
 aws configure set default.region ${AWS_REGION}
 aws configure get default.region
+aws configure set cli_pager ""
 
 echo "Resizing Cloud9 instance EBS Volume"
 sh ./scripts/resize-cloud9-ebs-vol.sh 40
