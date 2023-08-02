a4e883944cf8160c0876948a07825f1d3e30b355
Syed Hussain <suhussai@ualberta.ca>
2023-08-02 02:38:02 +0000

    add destroy policy and update workshop stack script

--- standalone-eks-stack/bin/saas-workshop.ts
+++ standalone-eks-stack/bin/saas-workshop.ts
@@ -2,15 +2,18 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { EksBlueprintStack } from "../lib/eks/eks-blueprint-stack";
+import { DestroyPolicySetter } from "../lib/cdk-aspect/destroy-policy-setter";
 
 const app = new cdk.App();
 const account = process.env.CDK_DEFAULT_ACCOUNT;
 const region = process.env.CDK_DEFAULT_REGION;
 
-new EksBlueprintStack(app, "eksBlueprintStack", {
+const eksBlueprintStack = new EksBlueprintStack(app, "eksBlueprintStack", {
   stackName: "saas-workshop-eksBlueprintStack",
   env: { account, region },
 });
 
 // Remove comment to display failed checks.
 // cdk.Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
+
+cdk.Aspects.of(eksBlueprintStack).add(new DestroyPolicySetter());
