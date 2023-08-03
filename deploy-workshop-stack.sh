9b68150c0d49a6eb3d3dfb147499431176e614a4
Syed Hussain <suhussai@ualberta.ca>
2023-08-03 23:29:36 +0000

    add IsWorkshopStudioEnv CFN template parameter, and update deploy-cluster.sh to use same method as WS to bootstrap workshop

--- deploy-workshop-stack.sh
+++ deploy-workshop-stack.sh
@@ -3,12 +3,18 @@
 corepack enable
 corepack prepare yarn@stable --activate
 
+# check if IS_WORKSHOP_STUDIO_ENV exists and is equal to "yes"
+export CREATE_CLOUD9_INSTANCE="false"
+if [ "$IS_WORKSHOP_STUDIO_ENV" = "yes" ]; then
+    export CREATE_CLOUD9_INSTANCE="true"
+fi
+
 echo "Starting cdk deploy..."
 cd standalone-eks-stack
 yarn install
 npx cdk bootstrap
 npx --yes cdk deploy eksBlueprintStack/EKSStack \
     --require-approval never \
-    --parameters EksBlueprintStack:createCloud9Instance=true
+    --parameters EksBlueprintStack:createCloud9Instance="$CREATE_CLOUD9_INSTANCE"
 
 echo "Done cdk deploy!"
