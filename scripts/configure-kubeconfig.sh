20c866873f02a8fdf36ef4202b143886581d2efb
Syed Hussain <suhussai@ualberta.ca>
2023-08-04 05:27:40 +0000

    fix configure kubeconfig script

--- scripts/configure-kubeconfig.sh
+++ scripts/configure-kubeconfig.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -x
 
-# STACK_NAME=$(aws cloudformation list-stacks --query "StackSummaries[?StackStatus=='CREATE_COMPLETE' || StackStatus=='UPDATE_COMPLETE'].StackName[?(contains(@,'EKSStack') || contains(@,'eksstack')) && ! contains(@,'Nested')]" --output text)
 STACK_NAME="SaaSWorkshopBootstrap"
-KUBECONFIG_COMMAND=$(aws cloudformation describe-stacks --stack-name "$STACK_NAME" --query "Stacks[0].Outputs[?contains(OutputKey,'EKSStackConfigCommand')].OutputValue" --output text)
+KUBECONFIG_COMMAND=$(aws cloudformation describe-stacks --stack-name "$STACK_NAME" --query "Stacks[0].Outputs[?contains(OutputKey,'ConfigCommand')].OutputValue" --output text)
 
 eval "$KUBECONFIG_COMMAND"
