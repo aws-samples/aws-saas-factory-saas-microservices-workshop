0288e32e6294e3e1aded3c43067bcfe41d809af6
Syed Hussain <suhussai@ualberta.ca>
2023-09-08 18:39:53 +0000

    update instance profile name in setup scripts

--- scripts/setup-cloud9.sh
+++ scripts/setup-cloud9.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 sudo yum -y install jq
-TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 10")
+
+TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 15")
 AWS_REGION=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
-C9_INSTANCE_ID=$(aws ec2 describe-instances --region $AWS_REGION --filters Name=tag:Name,Values=*Workshop-Instance* --query "Reservations[*].Instances[*].InstanceId" --output text)
-aws ec2 associate-iam-instance-profile --iam-instance-profile Name=saas-factory-microservices-workshop-admin --region $AWS_REGION --instance-id $C9_INSTANCE_ID
+C9_INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/instance-id)
+INSTANCE_PROFILE_NAME=$(aws ssm get-parameter --name "/saas-workshop/cloud9InstanceProfileName" --region "$AWS_REGION" --query 'Parameter.Value' --output text)
+aws ec2 associate-iam-instance-profile --iam-instance-profile Name="$INSTANCE_PROFILE_NAME" --region "$AWS_REGION" --instance-id "$C9_INSTANCE_ID"
