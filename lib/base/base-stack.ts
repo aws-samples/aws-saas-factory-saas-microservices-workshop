069749a318298c4c7d2249bced3e6b146856ae9c
Syed Hussain <suhussai@ualberta.ca>
2023-10-17 23:52:25 +0000

    update base stack to output base-image and create advanced tier eventbus and event-listener

--- lib/base/base-stack.ts
+++ lib/base/base-stack.ts
@@ -1,13 +1,19 @@
 import * as cdk from "aws-cdk-lib";
+import * as ssm from "aws-cdk-lib/aws-ssm";
+import * as aws_events from "aws-cdk-lib/aws-events";
+import * as aws_events_targets from "aws-cdk-lib/aws-events-targets";
+import * as logs from "aws-cdk-lib/aws-logs";
 import { Construct } from "constructs";
 import { EksCluster } from "../../lib/eks/eks-blueprint-stack";
 import { CognitoResources } from "../../lib/cognito/cognito-stack";
 import { IstioResources } from "../../lib/eks/istio-stack";
 import { CloudwatchAgentAddOnStack } from "../eks/cloudwatch-agent";
+import { Tier } from "../enums/tier";
 
 export interface BaseStackProps extends cdk.StackProps {
   tlsCertIstio: string;
   tlsKeyIstio: string;
+  workshopSSMPrefix: string;
 }
 
 export class BaseStack extends cdk.Stack {
@@ -15,16 +21,55 @@ export class BaseStack extends cdk.Stack {
   public readonly cognitoResources: CognitoResources;
   public readonly istioResources: IstioResources;
   public readonly cloudwatchAgentAddOnStack: CloudwatchAgentAddOnStack;
+  public readonly baseImage: string;
+  public readonly advancedTierEventBus: aws_events.EventBus;
 
   constructor(scope: Construct, id: string, props: BaseStackProps) {
     super(scope, id, props);
 
     const tlsCertIstio = props.tlsCertIstio;
     const tlsKeyIstio = props.tlsKeyIstio;
+    const workshopSSMPrefix = props.workshopSSMPrefix;
 
     const cognitoResources = new CognitoResources(this, "CognitoResources");
 
-    const eksCluster = new EksCluster(this, "EksCluster");
+    const eksCluster = new EksCluster(this, "EksCluster", {
+      workshopSSMPrefix: workshopSSMPrefix,
+    });
+
+    const baseImageSSMParameterName = `${workshopSSMPrefix}/sharedImageUri`;
+    this.baseImage = ssm.StringParameter.valueFromLookup(
+      this,
+      baseImageSSMParameterName
+    );
+
+    this.advancedTierEventBus = new aws_events.EventBus(
+      this,
+      "advanced-tier-event-bus"
+    );
+
+    // EVENT WATCHER START
+    // This rule allows us to read ALL events sent to the advancedTierEventBus
+    const eventBusWatcherRule = new aws_events.Rule(
+      this,
+      "EventBusWatcherRule",
+      {
+        eventBus: this.advancedTierEventBus,
+        enabled: true,
+        eventPattern: {
+          account: [cdk.Stack.of(this).account]
+        },
+      }
+    );
+    eventBusWatcherRule.addTarget(
+      new aws_events_targets.CloudWatchLogGroup(
+        new logs.LogGroup(this, "watcher-log-group", {
+          logGroupName: `${workshopSSMPrefix}/${Tier.Advanced}-event-bus-logs`,
+          retention: logs.RetentionDays.ONE_WEEK,
+        })
+      )
+    );
+    // EVENT WATCHER END
 
     const istioResources = new IstioResources(this, "IstioResources", {
       cluster: eksCluster.cluster,
@@ -39,6 +84,7 @@ export class BaseStack extends cdk.Stack {
       "CloudwatchAgentStack",
       {
         cluster: eksCluster.cluster,
+        workshopSSMPrefix: workshopSSMPrefix,
       }
     );
 
