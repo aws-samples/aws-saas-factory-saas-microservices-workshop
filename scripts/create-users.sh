5aadacdd57981ecd1634952d2a78c2423a03d9b5
Syed Hussain <suhussai@ualberta.ca>
2023-10-18 00:17:01 +0000

    update scripts to work for tenant-e and update run-queries script

--- scripts/create-users.sh
+++ scripts/create-users.sh
@@ -18,7 +18,7 @@ mkdir ./tmp
 echo "=================================================================================" >./tmp/Sample_JWTs.txt
 echo "Tenant Id, Tenant Tier, JWT" >>./tmp/Sample_JWTs.txt
 echo "=================================================================================" >>./tmp/Sample_JWTs.txt
-for ((u = 1; u <= 4; u++)); do
+for ((u = 1; u <= 5; u++)); do
     USER="user"${u}@example.com
     echo "Creating ${USER} in ${POOLNAME}"
 
@@ -33,6 +33,7 @@ for ((u = 1; u <= 4; u++)); do
     "2") tier="advanced" ;;
     "3") tier="premium" ;;
     "4") tier="basic" ;;
+    "5") tier="advanced" ;;
     *) tier="none" ;;
     esac
 
@@ -41,6 +42,7 @@ for ((u = 1; u <= 4; u++)); do
     "2") tenant_id="tenant-b" ;;
     "3") tenant_id="tenant-c" ;;
     "4") tenant_id="tenant-d" ;;
+    "5") tenant_id="tenant-e" ;;
     *) tenant_id="none" ;;
     esac
 
