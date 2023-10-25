5eab6d08625a0de29f4dc4b105dbcb1c209fe298
Syed Hussain <suhussai@ualberta.ca>
2023-10-25 22:25:14 +0000

    add script for simplifying on-your-own workshop setup flow

---
+++ standalone-eks-stack/deploy-workshop-resources.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -xe
+
+# This script is meant to be run as part of the
+# "on-your-own" flow of the saas microservices workshop.
+
+echo "Deploying workshop resources..."
+
+REPO_URL="https://github.com/aws-samples/aws-saas-factory-saas-microservices-workshop.git"
+REPO_BRANCH_NAME="v2"
+PARTICIPANT_ASSUMED_ROLE_ARN="$(aws sts get-caller-identity --query 'Arn' --output text)"
+
+# Use the same method that workshop studio uses to deploy standalone stack
+aws cloudformation deploy \
+    --template-file WorkshopStack.yaml \
+    --stack-name workshopStack \
+    --capabilities CAPABILITY_IAM \
+    --parameter-overrides \
+        RepoUrl="$REPO_URL" \
+        RepoBranchName="$REPO_BRANCH_NAME" \
+        ParticipantAssumedRoleArn="$PARTICIPANT_ASSUMED_ROLE_ARN"
+
+echo "Done deploying workshop resources!"
