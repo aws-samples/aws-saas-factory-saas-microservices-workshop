7d2d7db695dd9d745e26355ce24cd2c7cbd7a2f5
Syed Hussain <suhussai@ualberta.ca>
2023-10-25 23:17:36 +0000

    update deploy-workshop-resources with the appropriate parameters

--- standalone-eks-stack/deploy-workshop-resources.sh
+++ standalone-eks-stack/deploy-workshop-resources.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 
 # This script is meant to be run as part of the
 # "on-your-own" flow of the saas microservices workshop.
@@ -9,14 +9,13 @@ REPO_URL="https://github.com/aws-samples/aws-saas-factory-saas-microservices-wor
 REPO_BRANCH_NAME="v2"
 PARTICIPANT_ASSUMED_ROLE_ARN="$(aws sts get-caller-identity --query 'Arn' --output text)"
 
-# Use the same method that workshop studio uses to deploy standalone stack
 aws cloudformation create-stack \
-    --template-file WorkshopStack.yaml \
     --stack-name workshopStack \
+    --template-body file://WorkshopStack.yaml \
     --capabilities CAPABILITY_IAM \
-    --parameter-overrides \
-        RepoUrl="$REPO_URL" \
-        RepoBranchName="$REPO_BRANCH_NAME" \
-        ParticipantAssumedRoleArn="$PARTICIPANT_ASSUMED_ROLE_ARN"
+    --parameters \
+        ParameterKey=RepoUrl,ParameterValue="$REPO_URL" \
+        ParameterKey=RepoBranchName,ParameterValue="$REPO_BRANCH_NAME" \
+        ParameterKey=ParticipantAssumedRoleArn,ParameterValue="$PARTICIPANT_ASSUMED_ROLE_ARN"
 
 echo "CloudFormation stack creation started. You can monitor progress in the AWS Console at https://console.aws.amazon.com/cloudformation"
