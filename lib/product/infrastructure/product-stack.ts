35361c1f7f8ce107b7424a1d20f6abb0d5dd2b6b
Humberto Somensi <somensi@amazon.com>
2023-10-29 19:46:44 +0000

    removed service name variable from sidecar env vars

--- lib/product/infrastructure/product-stack.ts
+++ lib/product/infrastructure/product-stack.ts
@@ -286,7 +286,7 @@ export class ProductStack extends Construct {
                   },
                   {
                     name: "SERVICE_NAME",
-                    value: serviceName,
+                    value: "Product",
                   },
                   {
                     name: "POD_NAMESPACE",
