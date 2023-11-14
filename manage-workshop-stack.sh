136eb68d5b01621e4d4522abe4473ac141eacdfa
Syed Hussain <suhussai@ualberta.ca>
2023-11-14 22:19:07 +0000

    add retry in manage-workshop-stack.sh script

--- manage-workshop-stack.sh
+++ manage-workshop-stack.sh
@@ -32,54 +32,64 @@ run_ssm_command() {
     fi
 }
 
-STACK_OPERATION=$(echo "$1" | tr '[:upper:]' '[:lower:]')
-CLOUD9_INSTANCE_ID_PARAMETER_NAME="/workshop/cloud9InstanceId"
-GIT_REPO=$REPO_URL
-GIT_BRANCH=$REPO_BRANCH_NAME
-CDK_VERSION="2.106.1"
+main() {
+    CLOUD9_INSTANCE_ID_PARAMETER_NAME="/workshop/cloud9InstanceId"
+    GIT_REPO=$REPO_URL
+    GIT_BRANCH=$REPO_BRANCH_NAME
+    CDK_VERSION="2.106.1"
 
-# TARGET_USER="ec2-user"
-TARGET_USER="ubuntu"
-corepack enable || echo "default to yarn v1"
-corepack prepare yarn@3.6.4 --activate || echo "default to yarn v1"
+    # TARGET_USER="ec2-user"
+    TARGET_USER="ubuntu"
+    corepack enable || echo "default to yarn v1"
+    corepack prepare yarn@3.6.4 --activate || echo "default to yarn v1"
 
-cd standalone-eks-stack
-yarn install
-npx -y cdk@$CDK_VERSION bootstrap
+    cd standalone-eks-stack
+    yarn install
+    npx -y cdk@$CDK_VERSION bootstrap
 
-if [[ "$STACK_OPERATION" == "create" || "$STACK_OPERATION" == "update" ]]; then
-    echo "Starting cdk deploy..."
-    npx cdk@$CDK_VERSION deploy SaaSWorkshopBootstrap \
-        --require-approval never
-    echo "Done cdk deploy!"
+    if [[ "$STACK_OPERATION" == "create" || "$STACK_OPERATION" == "update" ]]; then
+        echo "Starting cdk deploy..."
+        npx cdk@$CDK_VERSION deploy SaaSWorkshopBootstrap \
+            --require-approval never
+        echo "Done cdk deploy!"
 
-    if [[ "$STACK_OPERATION" == "create" ]]; then
-        # get cloud9 instance id from ssm parameter store
+        if [[ "$STACK_OPERATION" == "create" ]]; then
+            # get cloud9 instance id from ssm parameter store
+            C9_PID=$(aws ssm get-parameter \
+                --name "$CLOUD9_INSTANCE_ID_PARAMETER_NAME" \
+                --output text \
+                --query "Parameter.Value")
+
+            run_ssm_command "$TARGET_USER" "$C9_PID" "cd ~/environment && git clone --single-branch --branch $GIT_BRANCH $GIT_REPO || echo 'Repo already exists.'"
+            run_ssm_command "$TARGET_USER" "$C9_PID" "rm -vf ~/.aws/credentials"
+            run_ssm_command "$TARGET_USER" "$C9_PID" "cd ~/environment/aws-saas-factory-saas-microservices-workshop && ./setup.sh"
+            run_ssm_command "$TARGET_USER" "$C9_PID" "cd ~/environment/aws-saas-factory-saas-microservices-workshop && ./deploy.sh"
+            aws ec2 reboot-instances --instance-ids "$C9_PID"
+        fi
+    elif [ "$STACK_OPERATION" == "delete" ]; then
         C9_PID=$(aws ssm get-parameter \
             --name "$CLOUD9_INSTANCE_ID_PARAMETER_NAME" \
             --output text \
-            --query "Parameter.Value")
+            --query "Parameter.Value" 2>/dev/null || echo "None")
 
-        run_ssm_command "$TARGET_USER" "$C9_PID" "cd ~/environment && git clone --single-branch --branch $GIT_BRANCH $GIT_REPO || echo 'Repo already exists.'"
-        run_ssm_command "$TARGET_USER" "$C9_PID" "rm -vf ~/.aws/credentials"
-        run_ssm_command "$TARGET_USER" "$C9_PID" "cd ~/environment/aws-saas-factory-saas-microservices-workshop && ./setup.sh"
-        run_ssm_command "$TARGET_USER" "$C9_PID" "cd ~/environment/aws-saas-factory-saas-microservices-workshop && ./deploy.sh"
-        aws ec2 reboot-instances --instance-ids "$C9_PID"
+        if [[ "$C9_PID" != "None" ]]; then
+            run_ssm_command "$TARGET_USER" "$C9_PID" "cd ~/environment/aws-saas-factory-saas-microservices-workshop && ./destroy.sh"
+        fi
+
+        echo "Starting cdk destroy..."
+        npx cdk@$CDK_VERSION destroy --all --force
+        echo "Done cdk destroy!"
+    else
+        echo "Invalid stack operation!"
+        exit 1
     fi
-elif [ "$STACK_OPERATION" == "delete" ]; then
-    C9_PID=$(aws ssm get-parameter \
-        --name "$CLOUD9_INSTANCE_ID_PARAMETER_NAME" \
-        --output text \
-        --query "Parameter.Value" 2>/dev/null || echo "None")
+}
 
-    if [[ "$C9_PID" != "None" ]]; then
-        run_ssm_command "$TARGET_USER" "$C9_PID" "cd ~/environment/aws-saas-factory-saas-microservices-workshop && ./destroy.sh"
-    fi
+STACK_OPERATION=$(echo "$1" | tr '[:upper:]' '[:lower:]')
 
-    echo "Starting cdk destroy..."
-    npx cdk@$CDK_VERSION destroy --all --force
-    echo "Done cdk destroy!"
-else
-    echo "Invalid stack operation!"
-    exit 1
-fi
+for i in {1..5}; do
+    echo "iteration number: $i"
+    main "$STACK_OPERATION" && break
+    sleep 15
+done
+
