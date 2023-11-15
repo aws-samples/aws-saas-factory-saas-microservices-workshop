5da41bf95e9f7573c94b345e160398be0c2e8c58
Syed Hussain <suhussai@ualberta.ca>
2023-11-15 18:09:14 +0000

    remove scripts and files in testing/ dir

--- scripts/testing/cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ~/environment/aws-saas-factory-saas-microservices-workshop
-./destroy.sh
-
-cd ~/environment/aws-saas-factory-saas-microservices-workshop/standalone-eks-stack
-./destroy-cluster.sh
-
--- scripts/testing/lab2_updates_unbreak.py
+++
@@ -1,8 +0,0 @@
-from replace_function import replace_in_file
-
-lab2_bug_original_str = "'S': 'tenant-a'"
-
-lab2_bug_update_str = "'S': tenantContext.tenant_id"
-
-replace_in_file(lab2_bug_original_str, lab2_bug_update_str,
-                "../../lib/product/app/code/app.py")
